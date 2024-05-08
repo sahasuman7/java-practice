a12aeba41ceac7c23e8b72e0c293826c67f95304
Suman Saha <contactmrsumansaha@gmail.com>
2024-05-08 11:37:27 +0530

    String Builder Basic Program

--- src/com/stringsTopic/builder.java
+++ src/com/stringsTopic/builder.java
@@ -2,10 +2,11 @@ package com.stringsTopic;
 
 public class builder {
     public static void main(String[] args) {
-        StringBuilder str = new StringBuilder("sUman");
+        StringBuilder str = new StringBuilder("$$ sUMAN sAHA $$");
 //        str.append(" Debolina");
 //        System.out.println(str);
-//
+
+//        Basic Function StringBuilder Practic
 //        str.setCharAt(5,'+');
 //        str.deleteCharAt(0);
 //        str.reverse();
@@ -14,21 +15,36 @@ public class builder {
 //        int x = (int) 'A';
 //        System.out.println(x);
 //        System.out.println(str);
-        for (int i=0;i<str.length();i++){
-            if (((int)str.charAt(i)) >= 97){
-                int a = ((int)str.charAt(i)) - 32;
-                char x = (char)a;
 
-                str.setCharAt(i,x);
-            }else {
-                int a = ((int)str.charAt(i)) + 32;
-                char x = (char)a;
 
-                str.setCharAt(i,x);
+//         Conert All upper case into lower and visa varsa
+//        for (int i=0;i<str.length();i++){
+//            if (str.charAt(i)==' '){
+//                str.setCharAt(i,' ');
+//            }
+//            else if (((int)str.charAt(i)) >= 97 && ((int)str.charAt(i))<=123 ){
+//                int a = ((int)str.charAt(i)) - 32;
+//                char x = (char)a;
+//
+//                str.setCharAt(i,x);
+//            }else if(((int)str.charAt(i)) >=65 && ((int)str.charAt(i))<=91) {
+//                int a = ((int)str.charAt(i)) + 32;
+//                char x = (char)a;
+//
+//                str.setCharAt(i,x);
+//
+//            }
+//            else {
+//                continue;
+//            }
+//        }
+//        System.out.println(str);
 
-            }
-        }
-        System.out.println(str);
+
+
+
+
+
 
 
 
